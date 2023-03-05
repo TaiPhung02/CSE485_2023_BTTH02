80f35509fbb47f040e01a6a9cf532c6d01de68c8
TaiPhung02 <2051063445@e.tlu.edu.vn>
2023-03-05 14:10:44 +0800

    hoan thien user

--- services/UserService.php
+++ services/UserService.php
@@ -14,9 +14,90 @@ class UserService
 
         $users = [];
         while ($row = $stmt->fetch()) {
-            $user = new Category($row['id_nguoidung'], $row['tai_khoan'], $row['mat_khau'],$row['quyen']);
+            $user = new User($row['id_nguoidung'], $row['tai_khoan'], $row['mat_khau'],$row['quyen']);
             array_push($users, $user);
         }
+        return $users;
+    }
+
+    // ======== add
+    public function addUser($account, $password, $role){
+  
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
+ 
+        $sql_add = "INSERT INTO `users`(`tai_khoan`, `mat_khau`, `quyen`) VALUES ('" . $account . "', '" . $password . "', '" . $role . "')";
+        $stmt_add = $conn->query($sql_add);
+ 
+        $sql_select = "SELECT * FROM users";
+        $stmt_select = $conn->query($sql_select);
+ 
+        $users = [];
+        while($row = $stmt_select->fetch()){
+            $user = new User($row['id_nguoidung'], $row['tai_khoan'], $row['mat_khau'], $row['quyen']);
+            array_push($users,$user);
+        }
+ 
+        return $users;
+    }
+    // ========= edit
+    public function get_id_User($id)
+    {
+        $dbConn = new DbConnection();
+        $conn = $dbConn->getConnection();
+ 
+        $sql = "SELECT * FROM users WHERE id_nguoidung = '" . $id . "'";
+        $stmt = $conn->query($sql);
+ 
+        $users = [];
+        while ($row = $stmt->fetch()) {
+            $user = new User($row['id_nguoidung'], $row['tai_khoan'], $row['mat_khau'], $row['quyen']);
+            array_push($users, $user);
+        }
+        return $users;
+    }
+ 
+ 
+    public function updateUser($id, $account, $password, $role){
+ 
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
+ 
+ 
+        $sql_update = "UPDATE `users` SET `tai_khoan`=' " . $account . " ', `mat_khau`=' " . $password . " ', `quyen`=' " . $role . " ' WHERE `id_nguoidung` = '" . $id . "' ";
+        $stmt_update = $conn->query($sql_update);
+ 
+        $sql_select = "SELECT * FROM users";
+        $stmt_select = $conn->query($sql_select);
+ 
+     
+        $users = [];
+        while($row = $stmt_select->fetch()){
+            $user = new User($row['id_nguoidung'], $row['tai_khoan'], $row['mat_khau'], $row['quyen']);
+            array_push($users,$user);
+        }
+ 
+        return $users;
+    }
+
+    //======= delete
+    public function deleteUser($id){
+  
+        $dbConn = new DBConnection();
+        $conn = $dbConn->getConnection();
+ 
+        $sql_delete = "DELETE FROM `users` WHERE `id_nguoidung` = '" . $id . "'";
+        $stmt_delete = $conn->query($sql_delete);
+ 
+        $sql_select = "SELECT * FROM users";
+        $stmt_select = $conn->query($sql_select);
+ 
+        $users = [];
+        while($row = $stmt_select->fetch()){
+            $user = new User($row['id_nguoidung'], $row['tai_khoan'], $row['mat_khau'], $row['quyen']);
+            array_push($users,$user);
+        }
+ 
         return $users;
     }
 }
