18479790c86db1088f4404ee6ca2815067b4a36a
TaiPhung02 <2051063445@e.tlu.edu.vn>
2023-03-05 14:36:00 +0800

    hiển thị danh sách article

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -1,11 +1,10 @@
 <?php
+include("services/ArticleService.php");
 class ArticleController{
-    // Hàm xử lý hành động index
     public function index(){
-        // Nhiệm vụ 1: Tương tác với Services/Models
-        echo "Tương tác với Services/Models from Article";
-        // Nhiệm vụ 2: Tương tác với View
-        echo "Tương tác với View from Article";
+        $articleService = new ArticleService();
+        $articles = $articleService->getAllArticle();
+        include("views/article/index.php");
     }
 
     public function add(){
